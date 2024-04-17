dedafa8164f0ec9eda77ba4cb006d1468f7acc3b
Diego Porras <diego.porras@owasp.org>
2024-04-17 23:43:48 +0100

    Await for children and propagate return code in the parent

--- app/main.c
+++ app/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
@@ -13,6 +14,7 @@ int main(int argc, char *argv[]) {
 	// Uncomment this block to pass the first stage
 	//
 	char *command = argv[3];
+	int status_code = 0;
 	int child_pid = fork();
 	if (child_pid == -1) {
 	    printf("Error forking!");
@@ -24,7 +26,8 @@ int main(int argc, char *argv[]) {
 	    execv(command, &argv[3]); //&argv[3] == <command> <arg1> <arg2> ...
 	} else {
 		   // We're in parent
-		   wait(NULL);
+		   wait(&status_code);
+		   exit(status_code);
 		   //printf("Child terminated");
 	}
 
